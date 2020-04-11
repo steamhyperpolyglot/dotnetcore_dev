ad0d4db1361a6fa67a07df1e87580afea016a0f8
Sherman Chen <uxdailygeek@gmail.com>
2020-04-11 17:34:46 +0800

    Added Code Examples for Chapter 11 & 12

---
+++ v2/Part3/NorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NorthwindWeb.Pages
+{
+	public class Suppliers : PageModel
+	{
+		public void OnGet()
+		{
+			
+		}
+	}
+}
---
+++ v2/Part3/NorthwindWeb/Pages/_ViewStart.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NorthwindWeb.Pages
+{
+	public class _ViewStart : PageModel
+	{
+		public void OnGet()
+		{
+			
+		}
+	}
+}
