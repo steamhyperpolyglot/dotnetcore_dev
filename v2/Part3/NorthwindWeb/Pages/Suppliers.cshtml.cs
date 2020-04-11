4864e6b661f5a3393228c5e1d5bcc82f80d8c864
Sherman Chen <uxdailygeek@gmail.com>
2020-04-11 17:35:05 +0800

    Completed Code Examples for Chapter 11 & 12

--- v2/Part3/NorthwindWeb/Pages/Suppliers.cshtml.cs
+++ v2/Part3/NorthwindWeb/Pages/Suppliers.cshtml.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace NorthwindWeb.Pages
 {
-	public class Suppliers : PageModel
+	public class SuppliersModel : PageModel
 	{
+		public IEnumerable <string> Suppliers { get; set; }
+
 		public void OnGet()
 		{
-			
+			ViewData [ "Title" ] = "Northwind Web Site - Suppliers";
+			Suppliers = new [] { "Alpha co", "Beta Limited", "Gamma Corp" };
 		}
 	}
 }
