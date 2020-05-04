043ff91f1be1000891cb0210975ea6500959ea2f
Sherman Chen <uxdailygeek@gmail.com>
2020-05-04 18:37:26 +0800

    Added a new code examples for WinForms

---
+++ v3/C# 8 and .NET Core 3 Projects/eBookManager/eBookManager.Engine/DeweyDecimal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eBookManager.Engine
+{
+	public class DeweyDecimal
+	{
+		public string ComputerScience { get; set; } = "000";
+		public string DataProcessing { get; set; } = "004";
+		public string ComputerProgramming { get; set; } = "005";
+	}
+}
---
+++ v3/C# 8 and .NET Core 3 Projects/eBookManager/eBookManager.Engine/StorageSpace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eBookManager.Engine
+{
+	[Serializable]
+	public class StorageSpace
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public List<Document> BookList { get; set; }
+	}
+}
