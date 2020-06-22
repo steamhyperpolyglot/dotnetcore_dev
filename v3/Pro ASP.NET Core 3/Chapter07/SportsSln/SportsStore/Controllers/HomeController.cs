10aa20eaa6e9de295be438cf779a3b13ae45abce
Sherman Chen <uxdailygeek@gmail.com>
2020-06-22 22:44:45 +0800

    Added additional code examples

---
+++ v3/Pro ASP.NET Core 3/Chapter07/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers {
+    public class HomeController : Controller {
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo) {
+            repository = repo;
+        }
+
+        public IActionResult Index() => View(repository.Products);
+    }
+}
