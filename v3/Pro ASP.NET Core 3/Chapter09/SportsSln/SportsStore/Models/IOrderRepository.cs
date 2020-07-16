ea1437d50dad644c6ab3351a022119ccdc23a2c2
Sherman Chen <uxdailygeek@gmail.com>
2020-07-16 11:00:24 +0800

    Completed code example for Chapter 9

--- v3/Pro ASP.NET Core 3/Chapter09/SportsSln/SportsStore/Models/IOrderRepository.cs
+++ v3/Pro ASP.NET Core 3/Chapter09/SportsSln/SportsStore/Models/IOrderRepository.cs
@@ -1,7 +1,10 @@
-﻿namespace SportsStore.Models
+﻿using System.Linq;
+
+namespace SportsStore.Models
 {
 	public interface IOrderRepository
 	{
-		
+		IQueryable <Order> Orders { get; }
+		void SaveOrder ( Order order );
 	}
 }
