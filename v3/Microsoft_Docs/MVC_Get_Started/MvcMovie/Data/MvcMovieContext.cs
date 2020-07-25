c21049c73cf8fa65689730485e38f8cb699adc72
Sherman Chen <uxdailygeek@gmail.com>
2020-07-25 21:39:07 +0800

    Finished the MVC tutorial

--- v3/Microsoft_Docs/MVC_Get_Started/MvcMovie/Data/MvcMovieContext.cs
+++ v3/Microsoft_Docs/MVC_Get_Started/MvcMovie/Data/MvcMovieContext.cs
@@ -1,7 +1,12 @@
-﻿namespace MvcMovie.Data
+﻿using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
 {
-	public class MvcMovieContext
+	public class MvcMovieContext : DbContext
 	{
-		
+		public MvcMovieContext(DbContextOptions<MvcMovieContext> options) : base(options) { }
+
+		public DbSet <Movie> Movie { get; set; }
 	}
 }
--- v3/Microsoft_Docs/MVC_Get_Started/MvcMovie/Models/Movie.cs
+++ v3/Microsoft_Docs/MVC_Get_Started/MvcMovie/Models/Movie.cs
@@ -1,7 +1,36 @@
-﻿namespace MvcMovie.Models
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcMovie.Models
 {
 	public class Movie
 	{
+		public int Id { get; set; }
 		
+		[StringLength(60, MinimumLength = 3)]
+		[Required]
+		public string Title { get; set; }
+
+		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+		[Display(Name = "Release Date")]
+		[DataType(DataType.Date)]
+		public DateTime ReleaseDate { get; set; }
+
+		[RegularExpression(@"[A-Z]+[a-zA-Z]*$")]
+		[Required]
+		[StringLength(30)]
+		public string Genre { get; set; }
+		
+		[Range(1, 100)]
+		[DataType(DataType.Currency)]
+		[Column(TypeName = "decimal(18, 2)")]
+		public decimal Price { get; set; }
+
+		[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+		[StringLength(5)]
+		[Required]
+		public string Rating { get; set; }
 	}
 }
